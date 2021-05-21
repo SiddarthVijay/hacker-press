bf3e50b10bbce8b1c4ce5a8e8953cd0447f55e00
Siddarth Vijay <vsiddarthvijay@gmail.com>
2021-05-21 17:12:01 +0530

    [BUGFIX] fixed authorization problem by sending auth token

--- src/index.js
+++ src/index.js
@@ -7,16 +7,30 @@ import * as serviceWorker from './serviceWorker';
 import {
   ApolloProvider,
   ApolloClient,
-  createHttpLink,
-  InMemoryCache
+  InMemoryCache,
+  ApolloLink,
+  HttpLink
 } from '@apollo/client';
 
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authorization token from local storage.
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTYyMTU5NDY2MX0.rjYtr0VaOfDXe6icf-j3IH0YMKVYMxC0Pq11YnLIXdw';
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      "Authorization": token
+    }
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
 });
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink), // Chain it with the HttpLink
   cache: new InMemoryCache()
 });
 
